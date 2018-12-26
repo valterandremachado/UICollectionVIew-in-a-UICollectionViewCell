e9e541a8ba9919dedad21e4235b5bab161eba3d8
valterandremachado <valterandremachadodinho@gmail.com>
2018-12-26 11:51:05 +0800

    Initial Commit

---
+++ collectionInsideOfCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionInsideOfCollection
+//
+//  Created by VALTER ANDRE MACHADO on 26/12/2018.
+//  Copyright © 2018 VALTER ANDRE MACHADO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
